e6aa5ca43532e7addaa8303634af9f77e8dff64a
Yuri Ribeiro <yuri.ribeiro@gmail.com>
2026-07-07 10:02:35 +0100

    Added Book model class

--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from .serializers import BookSerializer
+from .models import Book
 
 # view for /api/books
+
+
 class BookView(APIView):
+    """ List all books, or create a new book """
+    
     def get(self, request, *args, **kwargs):
-        return Response({"hello": "django"}, status=status.HTTP_200_OK)
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
     
 book_view = BookView.as_view()    
     
