5fd8db24764916688cbcc04c53752ffc10b07c31
Yuri Ribeiro <yuri.ribeiro@gmail.com>
2026-09-15 16:44:42 +0100

    feat: Added detailed book endpoint

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,42 +1,50 @@
 
 from rest_framework.test import APITestCase
 from django.urls import reverse
+
 from api.models import Book
 
 class BookViewTest(APITestCase):
+    
     def test_response_is_correct(self):
-        
         book = Book.objects.create(
-            title='Test Book', 
-            author='Test Author', 
-            description='Test Description'
-            )
-        
-        url = reverse('api:book-list')
+            title="Test Book",
+            author="Test Author",
+            description="Test Description"
+        )
+
+        url = reverse('api:books')
         response = self.client.get(url, format='json')
         assert response.status_code == 200
         first_book = response.data[0]
-        assert first_book['title'] == 'Test Book'
-        assert first_book['author'] == 'Test Author'
-        assert first_book['description'] == 'Test Description'
+        assert first_book['title'] == book.title
+        assert first_book['author'] == book.author
+        assert first_book['description'] == book.description
 
-    def test_response_returns_book_by_id(self):
+
+class BookDetailViewTest(APITestCase):
+
+    def test_returns_book_by_id(self):
         book = Book.objects.create(
-            title='Test Book',
-            author='Test Author',
-            description='Test Description'
+            title="Test Book",
+            author="Test Author",
+            description="Test Description"
+        )
+        other_book = Book.objects.create(
+            title="Other Book",
+            author="Other Author",
+            description="Other Description"
         )
 
         url = reverse('api:book-detail', kwargs={'pk': book.pk})
         response = self.client.get(url, format='json')
-
         assert response.status_code == 200
-        assert response.data['title'] == 'Test Book'
-        assert response.data['author'] == 'Test Author'
-        assert response.data['description'] == 'Test Description'
+        assert response.data['id'] == book.pk
+        assert response.data['title'] == book.title
+        assert response.data['author'] == book.author
+        assert response.data['description'] == book.description
 
-    def test_response_returns_not_found_for_unknown_book_id(self):
+    def test_returns_404_for_missing_book(self):
         url = reverse('api:book-detail', kwargs={'pk': 999})
         response = self.client.get(url, format='json')
-
         assert response.status_code == 404
